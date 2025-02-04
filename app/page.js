9aa4536bb08085fd0b7bf945c69d164e3d4beb1f
arturoaguileraa <arturoaguileraglez@gmail.com>
2025-02-04 07:45:42 +0000

    Fix: Delete darkmode

--- app/page.js
+++ app/page.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
 import BreakfastTracker from "../components/BreakfastTracker";
 
 export default function Home() {
-  return <BreakfastTracker />;
+  return (
+    <>
+      <Head>
+        <title>Gestor de Desayunos ☕</title>
+        <meta name="description" content="App para gestionar desayunos y pagos" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <BreakfastTracker />
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightblue: "#ccebf7",
+      },
+    },
   },
   plugins: [],
 };
